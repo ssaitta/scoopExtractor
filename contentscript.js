609d88a58714952b868e61c190575299cfbae24f
ssaitta <ssaitta13@gmail.com>
2019-04-15 11:45:43 -0400

    add support for scoop too

--- contentscript.js
+++ contentscript.js
@@ -1,5 +1,14 @@
-function main(){
-	window.alert(`The scoopId of this article is: ${document.querySelector("meta[name='articleid']").content}`)
+function main() {
+	const path = window.location.pathname.split("/");
+	if (path.includes("oakarticle")) {
+		window.alert(`The scoopId of this article is: ${path[3]}`);
+	} else {
+		window.alert(
+			`The scoopId of this article is: ${
+				document.querySelector("meta[name='articleid']").content
+			}`
+		);
+	}
 }
 
-main()
+main();
